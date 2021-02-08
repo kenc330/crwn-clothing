f8d509e7327d18c1759f632eca945232fec1b431
kennyc33 <kcc.333@outlook.com>
2021-02-07 18:01:12 -0700

    Added sign-in-and-sign-up form components.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/preview-collection/collection-preview";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor() {
